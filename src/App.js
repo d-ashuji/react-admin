757a0c15c5b4cd1086b8aa6a84ae6825802baa84
Ashu Kumar Gupta <ashu.gupta@teraidatalabs.com>
2022-06-22 18:06:44 +0530

    added backdrop (loader)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import MiniDrawer from './components/navbar/drawer';
 import Login from './pages/login';
 import Dashboard from './pages/login/Dashboard';
 import ResponsiveDrawer from './components/responsiveNavbar';
+import Backdrop from './components/backdrop'
 
 function App() {
-  return (
-    <div className="App" style={{ backgroundColor: '#f1f4f8' }}>
-      {/* <ResponsiveDrawer /> */}
-      <Dashboard />
-    </div>
-  );
+    return (
+        <div className="App" style={{ backgroundColor: '#f1f4f8' }}>
+            {/* <ResponsiveDrawer /> */}
+            <Dashboard />
+            <Backdrop state={true} />
+        </div>
+    );
 }
 
 export default App;
