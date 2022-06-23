c7a0d161e36cc61ca6591ef3a135e92ebcc76ff9
Ashu Kumar Gupta <ashu.gupta@teraidatalabs.com>
2022-06-23 13:56:07 +0530

    themesetting page added

---
+++ src/components/ThemeSetting/index.js
@@ -0,0 +1,7 @@
+const Index = {
+    "logoUrl": "https://media.glassdoor.com/sqll/2339254/terai-squarelogo-1639647460590.png",
+    "footerText": "Powered By",
+    "footerYear": true,
+}
+
+export default Index
